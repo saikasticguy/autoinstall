cd47dfe3546fefec00517af9fdc2c76e2d639d92
Saikrishna <55780430+saikasticguy@users.noreply.github.com>
2024-06-19 15:29:57 +0530

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Executing commands ..."
+echo "Installing Redshell ..."
 
 runcommand ()
 {
@@ -10,20 +10,21 @@ runcommand ()
   sudo apt --fix-broken install
 #Installing Ubuntu Gnome Desktop
   sudo apt install ubuntu-gnome-desktop -y
-  echo "Installing Gnome - Complete"
+  echo "Gnome Installed"
 #Installing Chrome Remote Desktop (CRD)
   curl -o /tmp/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && \
   sudo apt install -y /tmp/chrome-remote-desktop_current_amd64.deb
-  echo "Installing Chrome Remote Desktop - Complete"
+  echo "Chrome Remote Desktop Installed"
 #Installing Chrome Browser (latest)
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
-  echo "Installing Chrome - Complete"
+  echo "Chrome Installed"
 #Installing p7Zip Desktop
   sudo snap install p7zip-desktop
-  echo "Installing 7Zip - Complete"
+  echo "p7Zip Installed"
 #Installing HTTP Toolkit
   wget https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v1.17.1/HttpToolkit-1.17.1.deb
   sudo dpkg -i HttpToolkit-1.17.1.deb
+  echo "HTTP Tool Kit installed"
 #Updating Gnome and reinstalling CRD to fix the broken input issue
   sudo apt autoremove -y
   sudo apt update
